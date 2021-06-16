f1320963f78e1bb99fc515851737a38ab1a10e8e
Víctor Altamirano <v.a.i@hotmail.com>
2021-06-16 18:07:02 -0500

    commit

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# ReactJS2NextJS
- Tutorial Basico de como adaptar el proyecto basico de ReactJS con NextJS
+This is a starter template for [Learn Next.js](https://nextjs.org/learn).
---
+++ pages/App.js
@@ -0,0 +1,63 @@
+import React from "react"
+import Logo from './assets/logos/logo.svg';
+import styles from "../styles/App.module.css"
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state ={
+      text:<>Edit <code>src/App.js</code> and save to reload.</>
+    }
+
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      fetch("/api/hello")
+        .then(res => res.json())
+        .then(
+          (result) => {
+           console.log(result)
+           this.setState({
+             text:<>{result.date}</>
+           })
+          },
+          (error) => {
+            console.log(error)
+          }
+        )
+    }, 5000);
+
+  }
+
+  componentWillUnmount() {
+
+  }
+
+  render() {
+    return (
+      <div className={styles.App}>
+      <header className={styles.Appheader}>
+        <Logo className={styles.Applogo} />
+        <p>
+          {this.state.text}
+        </p>
+        <a
+          className={styles.Applink}
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+    );
+  }
+}
+
+App.propTypes = {
+
+};
+
+export default App;
